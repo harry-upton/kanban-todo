d951276f7c0461c7be06be5f1622dde989149960
OldMcSpud <harryupton100@gmail.com>
2020-10-18 11:57:45 +0100

    Reorganised root file structure

---
+++ kanban-todo/kanbantodo.py
@@ -0,0 +1,15 @@
+import sys
+from PyQt5 import QtWidgets as qtw
+from PyQt5 import QtCore as qtc
+from PyQt5 import QtGui  as qtg
+
+class MainWindow(qtw.QWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.show()
+
+if __name__ == "__main__":
+    app = qtw.QApplication(sys.argv)
+    w = MainWindow()
+    sys.exit(app.exec_())

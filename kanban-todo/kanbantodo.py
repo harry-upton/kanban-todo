f8e892f4cb02170266ee2b0e6f95a646476f9d28
Harry Upton <harryupton100@gmail.com>
2021-01-06 20:08:53 +0000

    Update kanbantodo.py

--- kanban-todo/kanbantodo.py
+++ kanban-todo/kanbantodo.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5 import QtWidgets as qtw
 from PyQt5 import QtCore as qtc
-from PyQt5 import QtGui  as qtg
+from PyQt5 import QtGui as qtg
 
 class MainWindow(qtw.QWidget):
     def __init__(self, *args, **kwargs):
